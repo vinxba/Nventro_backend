3d6729e9ef8442b4db176aa746514738f9a78232
Vinoba VM <vinx.git@gmail.com>
2026-06-08 21:29:44 +0530

    Delete Nventrobackend directory

--- Nventrobackend/fleet/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import VehicleListView, VehicleDetailView
-
-urlpatterns = [
-    path('vehicles/', VehicleListView.as_view(), name='vehicle-list'),
-    path('vehicles/<int:pk>/', VehicleDetailView.as_view(), name='vehicle-detail'),
-]
